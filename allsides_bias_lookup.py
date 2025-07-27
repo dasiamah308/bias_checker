acc1622afd8f36fa4912529bab4012c1a33d8fd4
Daniel Asiamah <danasiamah003@gmail.com>
2025-07-27 13:53:21 -0400

    setting up ML

---
+++ allsides_bias_lookup.py
@@ -0,0 +1,22 @@
+# AllSides Media Bias Lookup Table
+# Add more outlets as needed. The key is the domain (no https://, just the main domain), the value is the bias label.
+
+allsides_bias = {
+    "foxnews.com": "Right",
+    "nytimes.com": "Left",
+    "cnn.com": "Lean Left",
+    "wsj.com": "Center",
+    "breitbart.com": "Far Right",
+    "npr.org": "Center",
+    "thehill.com": "Center",
+    "msnbc.com": "Left",
+    "washingtonpost.com": "Lean Left",
+    "usatoday.com": "Center",
+    "newsmax.com": "Right",
+    "abcnews.go.com": "Center",
+    "cbsnews.com": "Center",
+    "nbcnews.com": "Lean Left",
+    "reuters.com": "Center",
+    # Add more outlets below
+    # "example.com": "BiasLabel",
+} 
